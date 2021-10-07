fd7ff841078b2b6e53ac503dbf22e555e5203f9e
Vi01etta <79164557+Vi01etta@users.noreply.github.com>
2021-10-07 12:49:59 +0300

    Update permissions.py

--- api_with_restrictions/advertisements/permissions.py
+++ api_with_restrictions/advertisements/permissions.py
@@ -5,7 +5,7 @@ class IsAuthOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
-        return request.user and request.user.is_authentificated
+        return request.user and request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
